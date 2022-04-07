ad4f346b6595b982eda8b4c1258e8b780de1c44c
Søren Heltborg Kristensen <62941288+sorenheltborgkristensen@users.noreply.github.com>
2022-04-07 20:14:23 +0200

    Fetch trending media data

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <trending-media></trending-media>
+</template>
+
+<script>
+import TrendingMedia from "./components/TrendingMedia.vue";
+
+export default {
+  components: {
+    TrendingMedia,
+  },
+};
+</script>
+
+<style></style>
---
+++ src/components/TrendingMedia.vue
@@ -0,0 +1,27 @@
+<template>
+  <section v-for="(trend, index) in trending" :key="index">
+    {{ trend.title }}
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      trending: [],
+    };
+  },
+
+  created() {
+    this.getTrendingMedia();
+  },
+
+  methods: {
+    async getTrendingMedia() {
+      const url = `${"https://api.themoviedb.org/3/trending/all/week?api_key=" + import.meta.env.VITE_API_KEY}`;
+      const data = await (await fetch(url)).json();
+      this.trending = data.results;
+    },
+  },
+};
+</script>
